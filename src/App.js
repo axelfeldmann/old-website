61babf4c878354ffe6b5c9a75e494afe9666b38a
Axel Feldmann <axel.s.feldmann@gmail.com>
2017-07-20 00:33:43 -0700

    minor tweeks and wipe css

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Footer from './components/footer';
 import store from './stores/store';
 import { Provider } from 'react-redux';
 
+import Tabs from './components/tabs';
+import TabPane from './components/tab-pane';
+
+import About from './components/tab-contents/about';
+import Projects from './components/tab-contents/projects';
+import Thoughts from './components/tab-contents/thoughts';
+
 const App = () => (
   <Provider store={store}>
     <div>
       <Header/>
+      <Tabs>
+        <TabPane label='About Me'><About/></TabPane>
+        <TabPane label='Projects'><Projects/></TabPane>
+        <TabPane label='Thoughts'><Thoughts/></TabPane>
+      </Tabs>
       <Footer/>
     </div>
   </Provider>
